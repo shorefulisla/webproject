7618e3aaac55361d5608b19185344d6c3c22fe8e
shoreful islam <abcd@gamil.com>
2025-01-20 19:25:46 +0600

    third and last

--- fst.cpp
+++ fst.cpp
@@ -74,5 +74,40 @@ signed main()
                
              }  
          }
+
+         //In the name of ALLAH
+         //bismillahir rahmanir rahim
+         
+         #include<bits/stdc++.h>
+         using namespace std;
+         #define int long long int
+         
+         #define pb push_back
+         #define endl '\n'
+         #define mod 1000000007
+          
+         const double eps = 1e-9;
+         const int inf = 2000000000;
+         const int infl = 9000000000000000000;
+          
+         #define mem(a,b) memset(a, b, sizeof(a) )
+         #define gcd(a,b) __gcd(a,b)
+         #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+         
+         int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+         int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+         
+         
+         signed main()
+         {
+             optimize();
+         
+             int TC;
+             cin>>TC;
+             while (TC--)
+             {
+               
+             }  
+         }
     }  
 }
